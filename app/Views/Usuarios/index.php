b2c66a87c11f3068b9bd544125b89e55bce64244
AmandaSFarias <amandasfarias15@gmail.com>
2024-05-28 20:02:32 -0300

    Trabalhando na camada de usuarios Template

--- app/Views/Usuarios/index.php
+++ app/Views/Usuarios/index.php
@@ -17,6 +17,7 @@
 <?php echo $this->section('conteudo') ?> 
 
 <div class="row">
+  
 <div class="col-lg-12">
                 <div class="block">
                   <div class="table-responsive"> 
@@ -48,15 +49,66 @@
 
 <script>
   $(document).ready(function() {
+
+    const DATATABLE_PTBR = {
+
+            "sEmptyTable": "Nenhum registro encontrado",
+            "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+            "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "_MENU_ resultados por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            },
+            "select": {
+                "rows": {
+                    "_": "Selecionado %d linhas",
+                    "0": "Nenhuma linha selecionada",
+                    "1": "Selecionado 1 linha"
+                }
+            }
+        }
+
+
     $('#ajaxTable').DataTable({
+
+      //tradução para o português
+    "oLanguage": DATATABLE_PTBR,
+
     "ajax": "<?php echo site_url('usuarios/recuperausuarios'); ?>",
     columns: [
         { "data": "imagem"},
         { "data": "nome"},
         { "data": "email"},
         { "data": "ativo"},
-    ]
+    ],
+
+    "deferRender": true,
+    "processing": true,
+    "language": {
+        processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Carregando...</span> '
+    },
+
+    "responsive": true,
+
+    //configuração para dispositivos móveis
+    "pagingType": $(window).width() < 768 ? "simple" : "simple_numbers",
+
   });
+
 });
 
 
